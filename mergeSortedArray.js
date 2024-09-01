be10e3915324efc8b706c7e0e1fb7bcc190b0e41
Yaroslav Pikiner <yaroslav6066@gmail.com>
2024-09-01 08:42:01 +0300

    Revert "Add forlders"
    This reverts commit 7ca3d6bf7f560a30b3eb65ae16bd39f16afe4dea.

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,23 +1,21 @@
-function mergeSortedArray(arr1: number[], m: number, arr2: number[], n: number) {
+function mergeSortedArray(arr1, m, arr2, m) {
   let first = m - 1;
   let second = n - 1;
   let i = m + n - 1;
 
   while(second >= 0) {
-    let fVal = arr1[first];
-    let sVal = arr2[second];
+    let fVal = nums1[first];
+    let sVal = nums2[second];
 
     if (fVal > sVal) {
-      arr1[i] = fVal;
+      nums1[i] = fVal;
       i--;
       first--;
     } else {
-      arr1[i] = sVal;
+      nums1[i] = sVal;
       i--;
       second--;
     }
-  }
 }
 
 console.log(mergeSortedArray([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
-
--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 
 function removeDuplicates(nums) {
     if (nums.length === 0) {
