26d70091ccf41a2995af0128b96e1b861bd7ba18
Yaroslav <33610056+HelloWoooooorld@users.noreply.github.com>
2024-09-01 08:19:45 +0300

    Create arrayInsertion.ts

---
+++ arrayInsertion.ts
@@ -0,0 +1,10 @@
+function insertArray(arr1: Array<number>, arr2: Array<number>): Array<number> {
+  const setArr = new Set<number>(arr1); // 
+
+
+  const res = arr2.filter((item) => setArr.has(item));
+
+  return res;
+}
+
+console.log(insertArray([1, 2, 3, 4], [3, 4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
